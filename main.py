96d3e591b3b8c998de11399aa5d0982e8312abe6
SouzaNicholas <76216808+SouzaNicholas@users.noreply.github.com>
2022-02-08 15:25:13 -0500

    Added function to create records in database for shows. It can accept any number of rows at once (Within reason).

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def main():
     data = fetch_top250(key)
     output_ratings(key, data)
     output_data(data)
+    create_show_records(db[1], data)
     close_db(db[0])
 
 
@@ -23,10 +24,9 @@ def fetch_series(key: str, title: str) -> dict:
 # I included a print statement here to illustrate an issue with the API.
 # The total rating only appears as 0. The only useful information returned
 # from a user rating API call is the spread of user votes on a 10-point scale
-def fetch_user_rating(key: str, id: str) -> str:
+def fetch_user_rating(key: str, id: str) -> dict:
     rating = requests.get(f"https://imdb-api.com/en/API/UserRatings/{key}/{id}")
-    print(rating.json())
-    return rating.json()["totalRating"]
+    return rating.json()
 
 
 def fetch_top250(key) -> dict:
@@ -48,13 +48,19 @@ def parse_json(data_dict: dict) -> dict:
 def output_ratings(key, data_dict):
     with open("titles.txt", 'w') as f:
         WoT = fetch_series(key, "The Wheel of Time")['results'][0]
-        f.write(WoT["title"] + " - Rating: " + fetch_user_rating(key, WoT["id"]) + '\n')
-        f.write(data_dict[1]["title"] + " - Rating: " + fetch_user_rating(key, data_dict[1]["id"]) + '\n')
-        f.write(data_dict[50]["title"] + " - Rating: " + fetch_user_rating(key, data_dict[50]["id"]) + '\n')
-        f.write(data_dict[100]["title"] + " - Rating: " + fetch_user_rating(key, data_dict[100]["id"]) + '\n')
-        f.write(data_dict[150]["title"] + " - Rating: " + fetch_user_rating(key, data_dict[150]["id"]) + '\n')
-        f.write(data_dict[200]["title"] + " - Rating: " + fetch_user_rating(key, data_dict[200]["id"]) + '\n')
-        f.write(data_dict[250]["title"] + " - Rating: " + fetch_user_rating(key, data_dict[250]["id"]) + '\n')
+        f.write(WoT["title"] + " - Rating: " + fetch_user_rating(key, WoT["id"])["totalRating"] + '\n')
+        f.write(data_dict[1]["title"] + " - Rating: " +
+                fetch_user_rating(key, data_dict[1]["id"])["totalRating"] + '\n')
+        f.write(data_dict[50]["title"] + " - Rating: " +
+                fetch_user_rating(key, data_dict[50]["id"])["totalRating"] + '\n')
+        f.write(data_dict[100]["title"] + " - Rating: " +
+                fetch_user_rating(key, data_dict[100]["id"])["totalRating"] + '\n')
+        f.write(data_dict[150]["title"] + " - Rating: " +
+                fetch_user_rating(key, data_dict[150]["id"])["totalRating"] + '\n')
+        f.write(data_dict[200]["title"] + " - Rating: " +
+                fetch_user_rating(key, data_dict[200]["id"])["totalRating"] + '\n')
+        f.write(data_dict[250]["title"] + " - Rating: " +
+                fetch_user_rating(key, data_dict[250]["id"])["totalRating"] + '\n')
 
 
 def output_data(data_dict):
@@ -116,6 +122,20 @@ def init_ratings_table(curs: sqlite3.Cursor):
                      one_percent REAL NOT NULL DEFAULT 0,
                      one_votes INTEGER NOT NULL DEFAULT 0,
                      FOREIGN KEY(imdbId) REFERENCES shows(imdbId));""")
+
+
+# Function expects a dict with dict objects as values.
+# This is how the JSON is parsed, so it's easiest to work with like this.
+def create_show_records(curs: sqlite3.Cursor, data: dict):
+    for row in data.values():
+        curs.execute("""INSERT INTO shows(imdbId, title, fullTitle, yr, crew, rating, ratingCount)
+                        VALUES (?, ?, ?, ?, ?, ?, ?)""", (row["id"],
+                                                          row["title"],
+                                                          row["fullTitle"],
+                                                          row["year"],
+                                                          row["crew"],
+                                                          row["imDbRating"],
+                                                          row["imDbRatingCount"]))
 
 
 if __name__ == '__main__':
